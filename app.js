aa11a28a42ffef2f7cefa3c5e379480f0c6982dc
Thwin Lin Htet <thwinlinhtetthwinlinhtet@gmail.com>
2023-07-07 15:55:56 +0630

    hello

---
+++ app.js
@@ -0,0 +1,30 @@
+// declare
+const inputOne = document.getElementById("one");
+const inputTwo = document.getElementById("two");
+const bOne = document.getElementById("b-one");
+const bTwo = document.getElementById("b-two");
+const bThree = document.getElementById("b-three");
+const bFour = document.getElementById("b-four");
+const finalResult = document.getElementById("finalResult");
+const results = document.getElementById("results");
+
+bOne.onclick = () => {
+  const resultOne = inputOne.value * inputTwo.value;
+  finalResult.innerText = `${inputOne.value} * ${inputTwo.value} = ${resultOne}`;
+  inputOne.value = inputTwo.value = null;
+};
+
+bTwo.onclick = () => {
+  const resultTwo = inputOne.value / inputTwo.value;
+  finalResult.innerText = `${inputOne.value} / ${inputTwo.value} = ${resultTwo}`;
+  inputOne.value = inputTwo.value = null;
+};
+
+bThree.onclick = () => {
+  results.innerHTML += `<li>${finalResult.innerText}</li>`;
+  finalResult.innerText = null;
+};
+
+bFour.onclick = () => {
+  finalResult.innerText = null;
+};
